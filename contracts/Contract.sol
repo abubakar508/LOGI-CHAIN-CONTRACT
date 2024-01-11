8f46ed6cf2477662ffea7d2360b028e4ec9cc128
wecbrams <97198443+wecbrams@users.noreply.github.com>
2024-01-11 23:54:11 +0300

    Update Contract.sol
     comments that explain the rationale behind certain decisions or any potential edge cases.

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0; //version of the solidity being used
 
 
 // name of the contract 
-
+// Contract for managing package logistics
 contract LogisticsContract {
     // enum package status deifne possible states a package can have
 
@@ -11,6 +11,7 @@ contract LogisticsContract {
 
 
 // defines a custom data structure of our smart contract 'PACKAGE"
+     // Data structure for the PACKAGE
     struct Package {
         address sender;
         address carrier;
@@ -20,15 +21,18 @@ contract LogisticsContract {
     }
 
 // creates mappig where package IDS ARE MAPPED to their respective package structs(structures)
+     // Mapping to store packages based on their IDs
     mapping(uint256 => Package) public packages;
     uint256 public packageCount;
 
 
 // event emitter: like in javascript: logs specific occurences on the blockchain
+    // Events to log package actions on the blockchain
     event PackageSent(uint256 packageId, address sender, address recipient);
     event PackageStatusChanged(uint256 packageId, PackageStatus status);
 
-// allows an externla caller to send a package to a different address(a recipient): takes recepient as an argument
+    // External function to send a package to a different address (recipient)
+  
     function sendPackage(address _recipient) external {
         uint256 newPackageId = packageCount++;
         Package storage newPackage = packages[newPackageId];
@@ -41,7 +45,7 @@ contract LogisticsContract {
     }
 
 
-// allows the sender, recipient, or assigned carrier to update the status of a package.
+    // Function to update the status of a package by the sender, recipient, or carrier
     function updatePackageStatus(uint256 _packageId, PackageStatus _status) external {
         require(_status >= PackageStatus.Pending && _status <= PackageStatus.Failed, "Invalid status");
 
@@ -53,7 +57,7 @@ contract LogisticsContract {
         emit PackageStatusChanged(_packageId, _status);
     }
 
-// allows the sender to assign a carrier for a package. 
+    // Function to allow the sender to assign a carrier for a package
     function assignCarrier(uint256 _packageId, address _carrier) external {
         Package storage package = packages[_packageId];
         require(package.sender == msg.sender, "Only sender can assign a carrier");
